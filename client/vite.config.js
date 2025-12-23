b4dcc9168d19ca3669c9d0861371430688a0df45
dagsteven <ton_email@gmail.com>
2025-12-23 13:54:00 +0100

    Transformation en PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Met à jour l'app automatiquement
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'TaskFlow - Gestion Sociale',
+        short_name: 'TaskFlow',
+        description: 'Gérez vos tâches et défiez vos amis.',
+        theme_color: '#111827', // La couleur de fond (gris foncé de ton app)
+        background_color: '#111827',
+        display: 'standalone', // Enlève la barre du navigateur (Plein écran)
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
